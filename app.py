2693e2dfcf1d59cdf18ba9954aa760a16d5e9fcb
abdul-usman <thewizardpoint@gmail.com>
2024-02-22 07:48:58 +0530

    init

--- app.py
+++ app.py
@@ -1,37 +1,12 @@
-def scrape_url(url):
-    try:
-        # Send an HTTP request to the URL
-        response = requests.get(
-            url,
-            headers={
-                'User-Agent':
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
-            })
+from flask import Flask, render_template
 
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            # Parse the HTML content of the page
-            soup = BeautifulSoup(response.text, 'html.parser')
+app = Flask(__name__)
 
-            # Extract the content of the first <h1> tag
-            h1_tag = soup.find('h1')
-            title = h1_tag.text.replace('\n', '').replace(
-                'GPL', '') if h1_tag else 'No <h1> tag found'
 
-            # Find the <div> containing the date information
-            time = soup.find('span', {'class': 'last-modified-timestamp'})
-            time_find = time.text.replace('\n', '') if time else 'not found'
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-            return {'time': time_find, 'title': title}
 
-        else:
-            return {
-                'url':
-                url,
-                'error':
-                f'Failed to retrieve the page. Status code: {response.status_code}'
-            }
-
-    except Exception as e:
-        print(f"Error scraping URL '{url}': {str(e)}")
-        return {'url': url, 'error': str(e)}
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
